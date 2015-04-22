85d704f6b3ea86a7357221c08c4f29f57c625a61
Mike <mike@prime31.com>
2015-04-22 14:59:24 -0700

    makes creating a new tween target even more trivial

---
+++ Assets/ZestKit/TweenTargets/AbstractTweenTarget.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace ZestKit
+{
+	/// <summary>
+	/// helper base class to make creating custom ITweenTargets as trivial as possible
+	/// </summary>
+	public abstract class AbstractTweenTarget<U,T> : ITweenTarget<T> where T : struct
+	{
+		protected U _target;
+
+		abstract public void setTweenedValue( T value );
+	}
+}
