c149459941854b8c8a78893e4519705e05ed3482
Mike <mike@prime31.com>
2015-04-24 16:54:56 -0700

    new demo scene!

---
+++ Assets/ZestKitDemo/EaseTypesSelector.cs
@@ -0,0 +1,242 @@
+﻿using UnityEngine;
+using System.Collections;
+using ZestKit;
+using System;
+
+
+public static class EaseTypesSelector
+{
+	static string[] _allEaseTypes;
+	static GUIContent[] comboBoxList;
+	static ComboBox comboBoxControl;
+	static GUIStyle listStyle = new GUIStyle();
+
+	static private bool _didRetinaIpadCheck;
+	static private bool _isRetinaIpad;
+
+
+	static bool isRetinaOrLargeScreen()
+	{
+		return Screen.width >= 960 || Screen.height >= 960;
+	}
+
+
+	static bool isRetinaIpad()
+	{
+		if( !_didRetinaIpadCheck )
+		{
+			if( Screen.height >= 2048 || Screen.width >= 2048 )
+				_isRetinaIpad = true;
+
+			_didRetinaIpadCheck = true;
+		}
+
+		return _isRetinaIpad;
+	}
+
+
+	static int buttonHeight()
+	{
+		if( isRetinaOrLargeScreen() )
+		{
+			if( isRetinaIpad() )
+				return 140;
+			return 70;
+		}
+
+		return 30;
+	}
+
+
+	static int buttonFontSize()
+	{
+		if( isRetinaOrLargeScreen() )
+		{
+			if( isRetinaIpad() )
+				return 40;
+			return 25;
+		}
+
+		return 15;
+	}
+
+
+	static EaseTypesSelector()
+	{
+		_allEaseTypes = Enum.GetNames( typeof( EaseType ) );
+
+		comboBoxList = new GUIContent[_allEaseTypes.Length];
+		for( var i = 0; i < _allEaseTypes.Length; i++ )
+			comboBoxList[i] = new GUIContent( _allEaseTypes[i] );
+
+		listStyle.normal.textColor = Color.white; 
+		listStyle.onHover.background = listStyle.hover.background = new Texture2D( 2, 2 );
+		listStyle.padding.left = listStyle.padding.right = listStyle.padding.top = listStyle.padding.bottom = 4;
+
+		comboBoxControl = new ComboBox( new Rect( Screen.width - 140, 20, 120, buttonHeight() ), comboBoxList[0], comboBoxList, "button", "box", listStyle );
+		comboBoxControl.SelectedItemIndex = 14;
+	}
+
+
+	public static void easeTypesGUI()
+	{
+		if( comboBoxControl.Show() )
+		{
+			var easeType = (EaseType)Enum.Parse( typeof( EaseType ), _allEaseTypes[comboBoxControl.SelectedItemIndex] );
+			ZestKit.ZestKit.defaultEaseType = easeType;
+		}
+	}
+
+
+	public static void setupGUIButtons()
+	{
+		GUI.skin.button.fontSize = buttonFontSize();
+		GUI.skin.button.margin = new RectOffset( 0, 0, 10, 0 );
+		GUI.skin.button.stretchWidth = true;
+		GUI.skin.button.fixedHeight = buttonHeight();
+		GUI.skin.button.wordWrap = false;
+		GUI.skin.button.active.textColor = Color.black;
+	}
+
+
+	public static float durationSlider( float duration )
+	{
+		GUILayout.BeginHorizontal();
+		GUILayout.Label( string.Format( "Duration: {0:0.0}", duration ), GUILayout.Width( 80 ) );
+		GUI.skin.horizontalSlider.margin = new RectOffset( 4, 4, 10, 4 );
+		var result = GUILayout.HorizontalSlider( duration, 0f, 2f, GUILayout.ExpandWidth( true ) );
+		GUILayout.EndHorizontal();
+
+		return result;
+	}
+}
+
+
+
+/*
+ * 
+// Popup list created by Eric Haines
+// ComboBox Extended by Hyungseok Seo.(Jerry) sdragoon@nate.com
+// Refactored by zhujiangbo jumbozhu@gmail.com
+// Slight edit for button to show the previously selected item AndyMartin458 www.clubconsortya.blogspot.com
+// 
+// -----------------------------------------------
+// This code working like ComboBox Control.
+// I just changed some part of code, 
+// because I want to seperate ComboBox button and List.
+// ( You can see the result of this code from Description's last picture )
+// -----------------------------------------------
+*/
+
+public class ComboBox
+{
+	private static bool forceToUnShow = false; 
+	private static int useControlID = -1;
+	private bool isClickedComboButton = false;
+	private int selectedItemIndex = 0;
+
+	private Rect rect;
+	private GUIContent buttonContent;
+	private GUIContent[] listContent;
+	private string buttonStyle;
+	private string boxStyle;
+	private GUIStyle listStyle;
+	Vector2 _scrollPos;
+
+
+	public ComboBox( Rect rect, GUIContent buttonContent, GUIContent[] listContent, GUIStyle listStyle )
+	{
+		this.rect = rect;
+		this.buttonContent = buttonContent;
+		this.listContent = listContent;
+		this.buttonStyle = "button";
+		this.boxStyle = "box";
+		this.listStyle = listStyle;
+	}
+
+	public ComboBox( Rect rect, GUIContent buttonContent, GUIContent[] listContent, string buttonStyle, string boxStyle, GUIStyle listStyle )
+	{
+		this.rect = rect;
+		this.buttonContent = buttonContent;
+		this.listContent = listContent;
+		this.buttonStyle = buttonStyle;
+		this.boxStyle = boxStyle;
+		this.listStyle = listStyle;
+	}
+
+	public bool Show()
+	{
+		if( forceToUnShow )
+		{
+			forceToUnShow = false;
+			isClickedComboButton = false;
+		}
+
+		bool result = false;
+		bool done = false;
+		int controlID = GUIUtility.GetControlID( FocusType.Passive );       
+
+		switch( Event.current.GetTypeForControl(controlID) )
+		{
+			case EventType.mouseUp:
+				{
+					if( isClickedComboButton )
+					{
+						done = true;
+					}
+				}
+				break;
+		}       
+
+		if( GUI.Button( rect, buttonContent, buttonStyle ) )
+		{
+			if( useControlID == -1 )
+			{
+				useControlID = controlID;
+				isClickedComboButton = false;
+			}
+
+			if( useControlID != controlID )
+			{
+				forceToUnShow = true;
+				useControlID = controlID;
+			}
+			isClickedComboButton = true;
+		}
+
+		if( isClickedComboButton )
+		{
+			Rect listRect = new Rect( rect.x, rect.y + listStyle.CalcHeight( listContent[0], 1.0f ),
+				rect.width, listStyle.CalcHeight( listContent[0], 1.0f ) * listContent.Length );
+
+			_scrollPos = GUI.BeginScrollView( new Rect( rect.xMin, rect.yMin + rect.height, rect.width + 18, Screen.height - 100f ), _scrollPos, listRect, false, true );
+			GUI.Box( listRect, "", boxStyle );
+
+			int newSelectedItemIndex = GUI.SelectionGrid( listRect, selectedItemIndex, listContent, 1, listStyle );
+			if( newSelectedItemIndex != selectedItemIndex )
+			{
+				selectedItemIndex = newSelectedItemIndex;
+				buttonContent = listContent[selectedItemIndex];
+				result = true;
+			}
+			GUI.EndScrollView( true );
+		}
+
+		if( done )
+			isClickedComboButton = false;
+
+		return result;
+	}
+
+
+	public int SelectedItemIndex
+	{
+		get{
+			return selectedItemIndex;
+		}
+		set{
+			selectedItemIndex = value;
+			buttonContent = listContent[selectedItemIndex];
+		}
+	}
+}
---
+++ Assets/ZestKitDemo/ZestKitDemo.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using ZestKit;
+
+
+public class ZestKitDemo : MonoBehaviour
+{
+	public Transform cube;
+	public AnimationCurve curve;
+	public RectTransform panel;
+
+	float _duration = 0.5f;
+
+
+	void OnGUI()
+	{
+		EaseTypesSelector.setupGUIButtons();
+		_duration = EaseTypesSelector.durationSlider( _duration );
+
+
+		if( GUILayout.Button( "Position Tween with 2 PingPong Loops" ) )
+		{
+			cube.ZKpositionTo( new Vector3( 9f, 5f ), _duration )
+				.setLoops( LoopType.PingPong, 2 )
+				.setLoopCompletionHandler( tw => Debug.Log( "Loop complete" ) )
+				.setCompletionHandler( tw => Debug.Log( "Tween complete" ) )
+				.start();
+		}
+
+
+		if( GUILayout.Button( "Relative Position Tween" ) )
+		{
+			cube.ZKpositionTo( new Vector3( 1f, 0f ), _duration )
+				.setIsRelative()
+				.start();
+		}
+
+		if( GUILayout.Button( "AnimationCurve for Easing Scale" ) )
+		{
+			cube.ZKlocalScaleTo( new Vector3( 3f, 3f, 3f ), _duration )
+				.setAnimationCurve( curve )
+				.start();
+		}
+
+
+		if( GUILayout.Button( "Scale back to 1" ) )
+		{
+			cube.ZKlocalScaleTo( new Vector3( 1f, 1f, 1f ), _duration )
+				.start();
+		}
+
+
+		if( GUILayout.Button( "Punch Scale to 3" ) )
+		{
+			cube.ZKlocalScaleTo( new Vector3( 3f, 3f, 3f ), _duration )
+				.setEaseType( EaseType.Punch )
+				.start();
+		}
+
+
+		if( GUILayout.Button( "Rotation to 270, 0, 0" ) )
+		{
+			cube.ZKlocalEulersTo( new Vector3( 270f, 0f ), _duration )
+				.start();
+		}
+
+
+		if( GUILayout.Button( "Rotation to 0, 310, 0" ) )
+		{
+			cube.ZKlocalEulersTo( new Vector3( 310f, 0f ), _duration )
+				.start();
+		}
+
+
+		if( GUILayout.Button( "Rotation by 0, 720, 360 (relative tween)" ) )
+		{
+			cube.ZKlocalEulersTo( new Vector3( 0f, 720f, 360f ), _duration )
+				.setIsRelative()
+				.start();
+		}
+
+
+		if( GUILayout.Button( "Material Color to Yellow with PingPong Loop" ) )
+		{
+			cube.GetComponent<Renderer>().material.ZKcolorTo( Color.yellow, _duration )
+				.setLoops( LoopType.PingPong )
+				.start();
+		}
+
+
+		if( GUILayout.Button( "RectTransform Panel Position Tween" ) )
+		{
+			panel.ZKanchoredPositionTo( new Vector2( -Screen.width * 0.5f, Screen.height * 0.5f ), _duration )
+				.setLoops( LoopType.PingPong )
+				.start();
+		}
+
+
+		if( GUILayout.Button( "RectTransform Button Position (relative tweens)" ) )
+		{
+			var leftButton = panel.GetChild( 0 ) as RectTransform;
+			var rightButton = panel.GetChild( 1 ) as RectTransform;
+
+			leftButton.ZKanchoredPositionTo( new Vector2( 0f, panel.rect.height * 0.6f ), _duration )
+				.setIsRelative()
+				.setLoops( LoopType.PingPong )
+				.start();
+
+			rightButton.ZKanchoredPositionTo( new Vector2( 0f, panel.rect.height * 0.6f ), _duration )
+				.setIsRelative()
+				.setLoops( LoopType.PingPong )
+				.setDelay( _duration * 0.5f )
+				.start();
+		}
+
+		EaseTypesSelector.easeTypesGUI();
+	}
+}
