993673d99eba6814ab4c09507d8ed326d426fb32
Eddie Cameron <eddie@weplaydots.com>
2017-03-29 17:23:43 -0400

    - put tabs in

--- Assets/ZestKit/ZestKit.cs
+++ Assets/ZestKit/ZestKit.cs
@@ -52,9 +52,9 @@ namespace Prime31.ZestKit
 		/// <summary>
 		/// stores tweens marked for removal
 		/// </summary>
-        List<ITweenable> _tempTweens = new List<ITweenable>();
+		List<ITweenable> _tempTweens = new List<ITweenable>();
 		List<ITweenable> _removedTweens = new List<ITweenable>();
-        int _nextTempTweenIdxToDelete = 0;
+		int _nextTempTweenIdxToDelete = 0;
 
 		/// <summary>
 		/// guard to stop instances being created while the application is quitting
@@ -143,24 +143,24 @@ namespace Prime31.ZestKit
 					_tempTweens.Add( tween );
 			}
 
-            // kill the dead Tweens
-            _nextTempTweenIdxToDelete = 0;
-            ITweenable tweenToDelete;
+			// kill the dead Tweens
+			_nextTempTweenIdxToDelete = 0;
+			ITweenable tweenToDelete;
 			for( var i = 0; i < _tempTweens.Count; i++ )
 			{
-                _nextTempTweenIdxToDelete++;
-                tweenToDelete = _tempTweens[i];
-                if ( _removedTweens.Contains( tweenToDelete ) )
-                    continue;
+				_nextTempTweenIdxToDelete++;
+				tweenToDelete = _tempTweens[i];
+				if ( _removedTweens.Contains( tweenToDelete ) )
+					continue;
 
 				tweenToDelete.recycleSelf();
 				_activeTweens.Remove( tweenToDelete );
 			}
 			_tempTweens.Clear();
-            _removedTweens.Clear();
-            _nextTempTweenIdxToDelete = -1;
+			_removedTweens.Clear();
+			_nextTempTweenIdxToDelete = -1;
 
-            _isUpdating = false;
+			_isUpdating = false;
 		}
 
 		#endregion
@@ -186,20 +186,20 @@ namespace Prime31.ZestKit
 		{
 			if( _isUpdating )
 			{
-                int tempTweensIdx = _tempTweens.IndexOf( tween );
-                if ( tempTweensIdx < 0 ) {
-                    // not in temp tweens yet, triggered by a tween stopping on its own
-                    tween.recycleSelf();
-                    _activeTweens.Remove( tween );
-                    _removedTweens.Add( tween );
-                }
-                else if ( tempTweensIdx >= _nextTempTweenIdxToDelete ) {
-                    // triggered by a tween being recycled
-                    tween.recycleSelf();
-                    _activeTweens.Remove( tween );
-                    _tempTweens.RemoveAt( tempTweensIdx );
-                }
-                // otherwise has already been deleted
+				int tempTweensIdx = _tempTweens.IndexOf( tween );
+				if ( tempTweensIdx < 0 ) {
+					// not in temp tweens yet, triggered by a tween stopping on its own
+					tween.recycleSelf();
+					_activeTweens.Remove( tween );
+					_removedTweens.Add( tween );
+				}
+				else if ( tempTweensIdx >= _nextTempTweenIdxToDelete ) {
+					// triggered by a tween being recycled
+					tween.recycleSelf();
+					_activeTweens.Remove( tween );
+					_tempTweens.RemoveAt( tempTweensIdx );
+				}
+				// otherwise has already been deleted
 			}
 			else
 			{
