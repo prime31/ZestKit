741ec9c3ef0b39308407943a0a416a555a98cb1d
Nesh108 <albertovaccari108@gmail.com>
2019-09-09 22:07:29 +0200

    added demo script and related assets

--- Assets/ZestKitDemo/Demo Scripts/ZestKitSplineDemo.cs
+++ Assets/ZestKitDemo/Demo Scripts/ZestKitSplineDemo.cs
@@ -1,83 +1,124 @@
-﻿using UnityEngine;
-using System.Collections;
-using Prime31.ZestKit;
+﻿using Prime31.ZestKit;
+using UnityEngine;
 
 
 public class ZestKitSplineDemo : MonoBehaviour
 {
-	public Transform quad;
+    public Transform quad;
+    [SerializeField] private bool LoadFromScriptableObject;
+    [SerializeField] private ZestKitSettings FigureEightSplineSettings;
+    [SerializeField] private ZestKitSettings CircleSplineSettings;
+    [SerializeField] private ZestKitSettings DemoRouteSplineSettings;
 
-	float _duration = 2.5f;
-
-
-	void OnGUI()
-	{
-		DemoGUIHelpers.setupGUIButtons();
-		_duration = DemoGUIHelpers.durationSlider( _duration, 5f );
-		DemoGUIHelpers.easeTypesGUI();
+    float _duration = 2.5f;
 
 
-		GUILayout.Label( "The splines used in this scene are on the\n*DummySpline GameObjects so you can\nhave a look at them." );
-		GUILayout.Label( "Just select the GameObject and the gizmos\nwill be drawn in the scene view." );
-		GUILayout.Space( 20 );
-
-		if( GUILayout.Button( "Figure Eight Spline Tween (relative)" ) )
-		{
-			var spline = new Spline( "figureEight" );
-			spline.closePath(); // we have to let the spline know it should close itself if using EaseTypes that go negative like Elastic to avoid clamping
-
-			new SplineTween( quad, spline, _duration )
-				.setIsRelative()
-				.start();
-		}
-			
-
-		if( GUILayout.Button( "Figure Eight Spline Tween (absolute)" ) )
-		{
-			var spline = new Spline( "figureEight" );
-			spline.closePath();
-
-			new SplineTween( quad, spline, _duration )
-				.start();
-		}
+    void OnGUI()
+    {
+        DemoGUIHelpers.setupGUIButtons();
+        _duration = DemoGUIHelpers.durationSlider(_duration, 5f);
+        DemoGUIHelpers.easeTypesGUI();
 
 
-		if( GUILayout.Button( "Circle Position Tween (relative with PingPong)" ) )
-		{
-			var spline = new Spline( "circle", true );
-			spline.closePath();
+        GUILayout.Label("The splines used in this scene are on the\n*DummySpline GameObjects so you can\nhave a look at them.");
+        GUILayout.Label("Just select the GameObject and the gizmos\nwill be drawn in the scene view.");
+        GUILayout.Space(20);
 
-			new SplineTween( quad, spline, _duration )
-				.setIsRelative()
-				.setLoops( LoopType.PingPong )
-				.start();
-		}
+        if (GUILayout.Button("Figure Eight Spline Tween (relative)"))
+        {
+            Spline spline;
+            if (LoadFromScriptableObject)
+            {
+
+                spline = new Spline("figureEight");
+            }
+            else
+            {
+                spline = new Spline(FigureEightSplineSettings.Nodes);
+            }
+
+            spline.closePath(); // we have to let the spline know it should close itself if using EaseTypes that go negative like Elastic to avoid clamping
+
+            new SplineTween(quad, spline, _duration)
+                .setIsRelative()
+                .start();
+        }
 
 
-		if( GUILayout.Button( "DemoRoute Tween (relative with PingPong)" ) )
-		{
-			var spline = new Spline( "demoRoute", true );
-			spline.closePath();
+        if (GUILayout.Button("Figure Eight Spline Tween (absolute)"))
+        {
+            Spline spline;
+            if (LoadFromScriptableObject)
+            {
 
-			new SplineTween( quad, spline, _duration )
-				.setIsRelative()
-				.setLoops( LoopType.PingPong )
-				.start();
-		}
+                spline = new Spline("figureEight");
+            }
+            else
+            {
+                spline = new Spline(FigureEightSplineSettings.Nodes);
+            }
+
+            spline.closePath();
+
+            new SplineTween(quad, spline, _duration)
+                .start();
+        }
 
 
-		if( GUILayout.Button( "Runtime Spline (relative with PingPong)" ) )
-		{
-			var nodes = new Vector3[] { new Vector3( 0, 0 ), new Vector3( 0, 0 ), new Vector3( 4f, 4f, 4f ), new Vector3( -4f, 5f, 6f ), new Vector3( -2f, 2f, 0f ), new Vector3( 0f, 0f ) };
-			var spline = new Spline( nodes );
-			spline.closePath();
+        if (GUILayout.Button("Circle Position Tween (relative with PingPong)"))
+        {
+            Spline spline;
+            if (LoadFromScriptableObject)
+            {
 
-			// setup the tween
-			new SplineTween( quad, spline, _duration )
-				.setIsRelative()
-				.setLoops( LoopType.PingPong )
-				.start();
-		}
+                spline = new Spline("circle");
+            }
+            else
+            {
+                spline = new Spline(CircleSplineSettings.Nodes);
+            }
+            spline.closePath();
 
-	}
+            new SplineTween(quad, spline, _duration)
+                .setIsRelative()
+                .setLoops(LoopType.PingPong)
+                .start();
+        }
+
+
+        if (GUILayout.Button("DemoRoute Tween (relative with PingPong)"))
+        {
+            Spline spline;
+            if (LoadFromScriptableObject)
+            {
+
+                spline = new Spline("demoRoute");
+            }
+            else
+            {
+                spline = new Spline(DemoRouteSplineSettings.Nodes);
+            }
+            spline.closePath();
+
+            new SplineTween(quad, spline, _duration)
+                .setIsRelative()
+                .setLoops(LoopType.PingPong)
+                .start();
+        }
+
+
+        if (GUILayout.Button("Runtime Spline (relative with PingPong)"))
+        {
+            var nodes = new Vector3[] { new Vector3(0, 0), new Vector3(0, 0), new Vector3(4f, 4f, 4f), new Vector3(-4f, 5f, 6f), new Vector3(-2f, 2f, 0f), new Vector3(0f, 0f) };
+            var spline = new Spline(nodes);
+            spline.closePath();
+
+            // setup the tween
+            new SplineTween(quad, spline, _duration)
+                .setIsRelative()
+                .setLoops(LoopType.PingPong)
+                .start();
+        }
+
+    }
 }
